7568f362cab98755f14e72d29168209fab52eecb
Michal Halabica <m.halabica@gmail.com>
2023-03-14 22:18:52 +0100

    CounterManager

---
+++ GrillBot.Core/Managers/Performance/CounterItem.cs
@@ -0,0 +1,21 @@
+﻿namespace GrillBot.Core.Managers.Performance;
+
+public sealed class CounterItem : IDisposable
+{
+    public Guid Id { get; set;  }
+    public string Section { get;  }
+    public DateTime StartAt { get; }
+    
+    private CounterManager CounterManager { get; }
+
+    public CounterItem(CounterManager counterManager, string section)
+    {
+        Id = Guid.NewGuid();
+        StartAt = DateTime.Now;
+        CounterManager = counterManager;
+        Section = section;
+    }
+
+    public void Dispose()
+        => CounterManager.Complete(this);
+}
