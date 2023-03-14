e17516df2ed006b50ff14c2dec247fe2ab6bcddf
Michal Halabica <m.halabica@gmail.com>
2023-03-14 22:56:07 +0100

    Database related features

---
+++ GrillBot.Core/Database/Repository/RepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using GrillBot.Core.Managers.Performance;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrillBot.Core.Database.Repository;
+
+public abstract class RepositoryBase<TContext> where TContext : DbContext
+{
+    protected TContext Context { get; }
+    private ICounterManager CounterManager { get; }
+
+    protected RepositoryBase(TContext context, ICounterManager counterManager)
+    {
+        Context = context;
+        CounterManager = counterManager;
+    }
+
+    protected CounterItem CreateCounter()
+        => CounterManager.Create($"Repository.{GetType().Name.Replace("Repository", "")}");
+}
