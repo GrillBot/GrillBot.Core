f321c87592d7ca660a43704d10663c9cde272367
Michal Halabica <m.halabica@gmail.com>
2023-06-16 23:22:52 +0200

    XSS Protection

--- GrillBot.Core/Infrastructure/CoreRequestFilter.cs
+++ GrillBot.Core/Infrastructure/CoreRequestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using GrillBot.Core.Services.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace GrillBot.Core.Infrastructure;
@@ -17,6 +18,8 @@ public class RequestFilter : IAsyncActionFilter
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
         var startAt = DateTime.Now;
+
+        SetXssProtection(context.HttpContext.Response);
         foreach (var filter in Filters)
             await filter.BeforeExecutionAsync();
 
@@ -24,6 +27,14 @@ public class RequestFilter : IAsyncActionFilter
 
         foreach (var filter in Filters)
             await filter.AfterExecutionAsync();
+
         await DiagnosticsManager.OnRequestEndAsync(context, result, startAt);
+    }
+
+    private static void SetXssProtection(HttpResponse response)
+    {
+        response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+        response.Headers.Add("X-Xss-Protection", "1; mode=block");
+        response.Headers.Add("X-Content-Type-Options", "nosniff");
     }
 }
