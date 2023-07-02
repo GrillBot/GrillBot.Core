2c92bab6071ac4beb453713f8c4ac5a6366fd959
Michal Halabica <m.halabica@gmail.com>
2023-07-02 19:12:36 +0200

    Added missing tests on ApiResult

--- GrillBot.Core.Tests/Infrastructure/Actions/ApiResultTests.cs
+++ GrillBot.Core.Tests/Infrastructure/Actions/ApiResultTests.cs
@@ -1,5 +1,6 @@
 ﻿using GrillBot.Core.Infrastructure.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GrillBot.Core.Tests.Infrastructure.Actions;
 
@@ -14,5 +15,38 @@ public class ApiResultTests
         Assert.IsNotNull(result);
         Assert.IsNull(result.Data);
         Assert.AreEqual(StatusCodes.Status200OK, result.StatusCode);
+    }
+
+    [TestMethod]
+    public void ToApiResult_IActionResult()
+    {
+        var apiResult = new ApiResult(StatusCodes.Status200OK, new PhysicalFileResult("file.png", "image/png"));
+        var result = apiResult.ToApiResult();
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<PhysicalFileResult>(result);
+    }
+
+    [TestMethod]
+    public void ToApiResult_NullData()
+    {
+        var apiResult = new ApiResult(StatusCodes.Status200OK);
+        var result = apiResult.ToApiResult();
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<StatusCodeResult>(result);
+        Assert.AreEqual(StatusCodes.Status200OK, ((StatusCodeResult)result).StatusCode);
+    }
+
+    [TestMethod]
+    public void ToApiResult_ObjectResult()
+    {
+        var apiResult = new ApiResult(StatusCodes.Status200OK, "Test");
+        var result = apiResult.ToApiResult();
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<ObjectResult>(result);
+        Assert.AreEqual(StatusCodes.Status200OK, ((ObjectResult)result).StatusCode);
+        Assert.AreEqual("Test", ((ObjectResult)result).Value);
     }
 }
