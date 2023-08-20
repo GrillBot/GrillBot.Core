9249ec09788c88f120a5f7fc91d104db15566961
Michal Halabica <m.halabica@gmail.com>
2023-08-20 17:59:27 +0200

    Unit tests on JSON helper

---
+++ GrillBot.Core.Tests/Helpers/JsonHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using GrillBot.Core.Helpers;
+using GrillBot.Core.Tests.TestCore;
+
+namespace GrillBot.Core.Tests.Helpers;
+
+[TestClass]
+public class JsonHelperTests
+{
+    [TestMethod]
+    public async Task SerializeAsync()
+    {
+        var data = new FlattenClass() { X = 5 };
+        var json = await JsonHelper.SerializeAsync(data);
+
+        Assert.IsNotNull(json);
+        Assert.AreNotEqual("{}", json);
+        Assert.AreEqual("{\"X\":5,\"SubItems\":null}", json);
+    }
+}
