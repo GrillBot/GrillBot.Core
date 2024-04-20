641ba34778b8851cc1850a827e09de259f01162d
Michal Halabica <m.halabica@gmail.com>
2024-04-20 19:43:58 +0200

    Support for timeouts in the UserMeasures service

--- GrillBot.Core.Services/UserMeasures/Models/Events/MemberWarningPayload.cs
+++ GrillBot.Core.Services/UserMeasures/Models/Events/MemberWarningPayload.cs
@@ -4,12 +4,15 @@ public class MemberWarningPayload : BasePayload
 {
     public override string QueueName => "user_measures:member_warning";
 
+    public bool SendDmNotification { get; set; }
+
     public MemberWarningPayload()
     {
     }
 
-    public MemberWarningPayload(DateTime createdAt, string reason, string guildId, string moderatorId, string targetUserId)
+    public MemberWarningPayload(DateTime createdAt, string reason, string guildId, string moderatorId, string targetUserId, bool sendDmNotification)
         : base(createdAt, reason, guildId, moderatorId, targetUserId)
     {
+        SendDmNotification = sendDmNotification;
     }
 }
