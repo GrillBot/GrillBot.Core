39e1709006b899662d893cb7f025d1d349e21c2a
Michal Halabica <m.halabica@gmail.com>
2024-05-11 11:46:41 +0200

    RabbitMQ event models for global error handling

---
+++ GrillBot.Core.Services/GrillBot/Models/Events/Errors/ErrorNotificationPayload.cs
@@ -0,0 +1,23 @@
+﻿using GrillBot.Core.RabbitMQ;
+
+namespace GrillBot.Core.Services.GrillBot.Models.Events.Errors;
+
+public class ErrorNotificationPayload : IPayload
+{
+    public string QueueName => "grillbot:error_notifications";
+
+    public string? Title { get; set; }
+    public List<ErrorNotificationField> Fields { get; set; } = new();
+    public ulong? UserId { get; set; }
+
+    public ErrorNotificationPayload()
+    {
+    }
+
+    public ErrorNotificationPayload(string? title, IEnumerable<ErrorNotificationField> fields, ulong? userId)
+    {
+        Title = title;
+        Fields = fields.Where(o => o is not null).ToList();
+        UserId = userId;
+    }
+}
