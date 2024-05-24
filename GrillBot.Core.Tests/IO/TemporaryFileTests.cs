53ee1e32a2a5b2b6d909305844e4dd5dce458464
Michal Halabica <m.halabica@gmail.com>
2024-05-24 22:22:10 +0200

    New TemporaryFile features

---
+++ GrillBot.Core.Tests/IO/TemporaryFileTests.cs
@@ -0,0 +1,103 @@
+﻿using GrillBot.Core.IO;
+using Microsoft.Testing.Platform.Extensions.Messages;
+
+namespace GrillBot.Core.Tests.IO;
+
+[TestClass]
+public class TemporaryFileTests
+{
+    private static async Task InitFileAsync(TemporaryFile file)
+        => await File.WriteAllTextAsync(file.Path, "ASDF");
+
+    [TestMethod]
+    public async Task CreateAndDisposeAsync()
+    {
+        var file = new TemporaryFile("txt");
+        await InitFileAsync(file);
+
+        file.Dispose();
+        Assert.IsFalse(File.Exists(file.Path));
+    }
+
+    [TestMethod]
+    public void ChangeExtension()
+    {
+        const string newExtension = "jpg";
+
+        using var file = new TemporaryFile("txt");
+        file.ChangeExtension(newExtension);
+
+        Assert.AreEqual($".{newExtension}", Path.GetExtension(file.Path));
+    }
+
+    [TestMethod]
+    public async Task ReadAllBytesAsync()
+    {
+        using var file = new TemporaryFile("txt");
+        await InitFileAsync(file);
+
+        var result = await file.ReadAllBytesAsync();
+        Assert.AreEqual(4, result.Length);
+    }
+
+    [TestMethod]
+    public async Task ReadAllLinesAsync()
+    {
+        using var file = new TemporaryFile("txt");
+        await InitFileAsync(file);
+
+        var result = await file.ReadAllLinesAsync();
+        Assert.AreEqual(1, result.Length);
+    }
+
+    [TestMethod]
+    public async Task ReadAllTextAsync()
+    {
+        using var file = new TemporaryFile("txt");
+        await InitFileAsync(file);
+
+        var result = await file.ReadAllTextAsync();
+        Assert.AreEqual(4, result.Length);
+    }
+
+    [TestMethod]
+    public async Task WriteAllBytesAsync()
+    {
+        using var file = new TemporaryFile("txt");
+
+        await file.WriteAllBytesAsync(new byte[] { 1, 2, 3, 4 });
+        var result = await file.ReadAllBytesAsync();
+
+        Assert.AreEqual(4, result.Length);
+    }
+
+    [TestMethod]
+    public async Task WriteAllLinesAsync()
+    {
+        using var file = new TemporaryFile("txt");
+
+        await file.WriteAllLinesAsync(new[] { "1" });
+        var result = await file.ReadAllLinesAsync();
+
+        Assert.AreEqual(1, result.Length);
+    }
+
+    [TestMethod]
+    public async Task WriteAllTextAsync()
+    {
+        using var file = new TemporaryFile("txt");
+
+        await file.WriteAllTextAsync("text");
+        var result = await file.ReadAllTextAsync();
+
+        Assert.AreEqual("text", result);
+    }
+
+    [TestMethod]
+    public void FilenameProperty()
+    {
+        using var file = new TemporaryFile("txt");
+
+        Assert.IsFalse(string.IsNullOrEmpty(file.Filename));
+    }
+}
--- GrillBot.Core/IO/TemporaryFile.cs
+++ GrillBot.Core/IO/TemporaryFile.cs
@@ -4,6 +4,8 @@ public sealed class TemporaryFile : IDisposable
 {
     public string Path { get; private set; }
 
+    public string Filename => System.IO.Path.GetFileName(Path);
+
     public TemporaryFile(string extension)
     {
         Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"{System.IO.Path.GetRandomFileName()}.{extension}");
@@ -11,6 +13,13 @@ public sealed class TemporaryFile : IDisposable
 
     public void ChangeExtension(string newExtension)
         => Path = System.IO.Path.ChangeExtension(Path, newExtension);
+
+    public Task<byte[]> ReadAllBytesAsync() => File.ReadAllBytesAsync(Path);
+    public Task<string[]> ReadAllLinesAsync() => File.ReadAllLinesAsync(Path);
+    public Task<string> ReadAllTextAsync() => File.ReadAllTextAsync(Path);
+    public Task WriteAllBytesAsync(byte[] bytes) => File.WriteAllBytesAsync(Path, bytes);
+    public Task WriteAllLinesAsync(IEnumerable<string> lines) => File.WriteAllLinesAsync(Path, lines);
+    public Task WriteAllTextAsync(string content) => File.WriteAllTextAsync(Path, content);
 
     public void Dispose()
     {
