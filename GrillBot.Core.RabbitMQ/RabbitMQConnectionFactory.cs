26742af038e1f99b6b5a244290bdb53387a9eeec
Michal Halabica <m.halabica@gmail.com>
2024-08-02 15:14:35 +0200

    RabbitMQ connection string

--- GrillBot.Core.RabbitMQ/RabbitMQConnectionFactory.cs
+++ GrillBot.Core.RabbitMQ/RabbitMQConnectionFactory.cs
@@ -5,25 +5,49 @@ namespace GrillBot.Core.RabbitMQ;
 
 public class RabbitMQConnectionFactory
 {
-    private IConfiguration Configuration { get; }
+    private readonly IConfiguration _configuration;
 
     public RabbitMQConnectionFactory(IConfiguration configuration)
     {
-        Configuration = configuration;
+        _configuration = configuration;
     }
 
     public IConnection Create()
     {
-        var configuration = Configuration.GetSection("RabbitMQ");
-        var factory = new ConnectionFactory
+        var connectionFactory =
+            CreateViaConnectionString() ??
+            CreateViaParameters() ??
+            throw new ArgumentException("Unable to find RabbitMQ connection string or parameters.");
+
+        connectionFactory.DispatchConsumersAsync = true;
+        connectionFactory.RequestedHeartbeat = TimeSpan.FromSeconds(30);
+
+        return connectionFactory.CreateConnection();
+    }
+
+    private ConnectionFactory? CreateViaConnectionString()
+    {
+        var connectionString = _configuration.GetConnectionString("RabbitMQ");
+        if (string.IsNullOrEmpty(connectionString))
+            return null;
+
+        return new ConnectionFactory
+        {
+            Uri = new Uri(connectionString)
+        };
+    }
+
+    private ConnectionFactory? CreateViaParameters()
+    {
+        var configuration = _configuration.GetSection("RabbitMQ");
+        if (!configuration.Exists())
+            return null;
+
+        return new ConnectionFactory
         {
             HostName = configuration["Hostname"],
             Password = configuration["Password"],
-            UserName = configuration["Username"],
-            DispatchConsumersAsync = true,
-            RequestedHeartbeat = TimeSpan.FromSeconds(30)
+            UserName = configuration["Username"]
         };
-
-        return factory.CreateConnection();
     }
 }
