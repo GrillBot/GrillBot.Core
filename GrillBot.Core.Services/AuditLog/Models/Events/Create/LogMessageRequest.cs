4a7b2c7fb6f7c851716e551128a8fb0715e1973e
Michal Halabica <m.halabica@gmail.com>
2025-04-06 21:07:10 +0200

    Dropped LogSeverity property in the AuditLog rabbit events

--- GrillBot.Core.Services/AuditLog/Models/Events/Create/LogMessageRequest.cs
+++ GrillBot.Core.Services/AuditLog/Models/Events/Create/LogMessageRequest.cs
@@ -1,11 +1,8 @@
-﻿using Discord;
-
-namespace GrillBot.Core.Services.AuditLog.Models.Events.Create;
+﻿namespace GrillBot.Core.Services.AuditLog.Models.Events.Create;
 
 public class LogMessageRequest
 {
     public string Message { get; set; } = null!;
-    public LogSeverity Severity { get; set; }
     public string SourceAppName { get; set; } = null!;
     public string Source { get; set; } = null!;
 
@@ -13,10 +10,9 @@ public class LogMessageRequest
     {
     }
 
-    public LogMessageRequest(string message, LogSeverity severity, string sourceAppName, string source)
+    public LogMessageRequest(string message, string sourceAppName, string source)
     {
         Message = message;
-        Severity = severity;
         SourceAppName = sourceAppName;
         Source = source;
     }
