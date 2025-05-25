f555192ff6ad7dfe4390c8d656cb95fded465b66
Michal Halabica <m.halabica@gmail.com>
2025-05-25 14:39:50 +0200

    Added missing telemetry service registration

--- GrillBot.Core.Metrics/TelemetryExtensions.cs
+++ GrillBot.Core.Metrics/TelemetryExtensions.cs
@@ -59,6 +59,8 @@ public static class TelemetryExtensions
             provider => provider.GetRequiredService<IMeterFactory>().Create(METER_NAME)
         );
 
+        builder.Services.AddHostedService<TelemetryService>();
+
         return builder;
     }
 
