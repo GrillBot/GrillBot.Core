64c8669a024fd5a5879d7b30134367a45c778551
Michal Halabica <m.halabica@gmail.com>
2025-05-30 21:05:32 +0200

    New extension methods to convert stream to bytes

---
+++ GrillBot.Core.Tests/Extensions/StreamExtensions.cs
@@ -0,0 +1,31 @@
+﻿using GrillBot.Core.Extensions;
+
+namespace GrillBot.Core.Tests.Extensions;
+
+[TestClass]
+public class StreamExtensions
+{
+    [TestMethod]
+    public void ToByteArray()
+    {
+        var sourceData = new byte[] { 1, 2, 3 };
+
+        using var sourceStream = new MemoryStream(sourceData);
+        var result = sourceStream.ToByteArray();
+
+        Assert.IsNotNull(result);
+        Assert.IsTrue(result.SequenceEqual(sourceData));
+    }
+
+    [TestMethod]
+    public async Task ToByteArrayAsync()
+    {
+        var sourceData = new byte[] { 1, 2, 3 };
+
+        await using var sourceStream = new MemoryStream(sourceData);
+        var result = await sourceStream.ToByteArrayAsync();
+
+        Assert.IsNotNull(result);
+        Assert.IsTrue(result.SequenceEqual(sourceData));
+    }
+}
---
+++ GrillBot.Core/Extensions/StreamExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace GrillBot.Core.Extensions;
+
+public static class StreamExtensions
+{
+    public static byte[] ToByteArray(this Stream stream)
+    {
+        using var ms = new MemoryStream();
+        stream.CopyTo(ms);
+
+        return ms.ToArray();
+    }
+
+    public static async Task<byte[]> ToByteArrayAsync(this Stream stream)
+    {
+        await using var ms = new MemoryStream();
+        await stream.CopyToAsync(ms);
+
+        return ms.ToArray();
+    }
+}
