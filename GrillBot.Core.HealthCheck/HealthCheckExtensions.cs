ed14a1aa707aac9f3acff7f53c3e9bd54c6c7bab
Michal Halabica <m.halabica@gmail.com>
2025-07-03 08:37:37 +0200

    Created package for healthchecks

---
+++ GrillBot.Core.HealthCheck/HealthCheckExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GrillBot.Core.HealthCheck;
+
+public static class HealthCheckExtensions
+{
+    public static IHealthChecksBuilder AddHealthChecks(this IServiceCollection services)
+    {
+        return HealthCheckServiceCollectionExtensions.AddHealthChecks(services);
+    }
+
+    public static IEndpointConventionBuilder MapHealthChecks(this IEndpointRouteBuilder endpoints, string pattern = "/health")
+    {
+        var options = new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = HealthCheckWriter.WriteResponseAsync
+        };
+
+        return HealthCheckEndpointRouteBuilderExtensions.MapHealthChecks(endpoints, pattern, options);
+    }
+}
