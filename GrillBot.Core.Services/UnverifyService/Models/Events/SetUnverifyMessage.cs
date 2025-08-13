c8b7dd387af7488c47070ba67366f7d1da1894b9
Michal Halabica <m.halabica@gmail.com>
2025-08-13 23:30:35 +0200

    Created constructor for simply creating of message

--- GrillBot.Core.Services/UnverifyService/Models/Events/SetUnverifyMessage.cs
+++ GrillBot.Core.Services/UnverifyService/Models/Events/SetUnverifyMessage.cs
@@ -7,4 +7,21 @@ public class SetUnverifyMessage : UnverifyRequest, IRabbitMessage
 {
     public string Topic => "Unverify";
     public string Queue => "SetUnverify";
+
+    public SetUnverifyMessage()
+    {
+    }
+
+    public SetUnverifyMessage(UnverifyRequest request)
+    {
+        GuildId = request.GuildId;
+        UserId = request.UserId;
+        ChannelId = request.ChannelId;
+        MessageId = request.MessageId;
+        EndAtUtc = request.EndAtUtc;
+        Reason = request.Reason;
+        TestRun = request.TestRun;
+        IsSelfUnverify = request.IsSelfUnverify;
+        RequiredKeepables = request.RequiredKeepables ?? [];
+    }
 }
