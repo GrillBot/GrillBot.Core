a03c3f44018a8219fbf9027df036ec1f32b3ed2f
Michal Halabica <m.halabica@gmail.com>
2025-09-18 21:54:13 +0200

    Fixed RubbergodService interface

--- GrillBot.Core.Services/RubbergodService/IRubbergodServiceClient.cs
+++ GrillBot.Core.Services/RubbergodService/IRubbergodServiceClient.cs
@@ -14,7 +14,7 @@ public interface IRubbergodServiceClient : IServiceClient
     Task<PaginatedResponse<UserKarma>> GetKarmaPageAsync([Query] PaginatedParams parameters, CancellationToken cancellationToken = default);
 
     [Get("/api/pins/{guildId}/{channelId}")]
-    Task<byte[]> GetPinsAsync(ulong guildId, ulong channelId, [Query] bool markdown, CancellationToken cancellationToken = default);
+    Task<HttpContent> GetPinsAsync(ulong guildId, ulong channelId, [Query] bool markdown, CancellationToken cancellationToken = default);
 
     [Get("/api/help/slashcommands")]
     Task<Dictionary<string, Cog>> GetSlashCommandsAsync(CancellationToken cancellationToken = default);
